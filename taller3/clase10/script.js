e193010eb52aadb40d3d02545d47dfcfc5dd8cc3
TacosConChelas <adriherandezor@gmail.com>
2024-04-25 07:32:51 -0600

    Se termino la clase 11

--- taller3/clase10/script.js
+++ taller3/clase10/script.js
@@ -1,1 +1,14 @@
-console.log("Hi, soy adriel y esta es una de las prubas para la clase");
+console.log("Hola este es un mensaje de prueba");
+const h1 = document.querySelector('h1');
+const p1 = document.querySelector('.parrafito');
+const p2 = document.querySelector('#pid');
+
+const input = document.querySelector('input');
+console.log({
+    h1, p1, p2, input
+});
+
+console.log(h1);
+console.log(input.value);
+
+
