8094656fe69119c3bc47fe3a94f21489e8fbf705
TacosConChelas <adriherandezor@gmail.com>
2024-05-01 18:08:07 -0600

    Se agregaron cambios en la carpeta clase10

--- taller3/clase10/script.js
+++ taller3/clase10/script.js
@@ -10,5 +10,17 @@ console.log({
 
 console.log(h1);
 console.log(input.value);
+/**
+ La propiedad .innerHTML nos permite agregar texto a la etiqueta y además colocar etiquetas 
+ o código html desde JS.
+ */ 
+h1.innerHTML = "Hola <br> Soy Adriel";
+p1.innerText = "Hoy es primero de mayo del <br> 2024";
 
+//console.log(p2.getAttribute("class"));
+//p2.setAttribute("class", "megaParrafo");
 
+p2.classList.add("negro");
+p2.classList.remove("negro");
+
+input.value = "22";
